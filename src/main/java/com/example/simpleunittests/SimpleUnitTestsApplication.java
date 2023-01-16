b99243edd44c1eb13673cba5b84c0eaca09925b8
Qobete <104554205+Qobete@users.noreply.github.com>
2023-01-16 10:27:47 +0200

    InitCommit

---
+++ src/main/java/com/example/simpleunittests/SimpleUnitTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleunittests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleUnitTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleUnitTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simpleunittests/SimpleUnitTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleunittests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleUnitTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
